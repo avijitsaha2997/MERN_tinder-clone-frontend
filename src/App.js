87a2e50fbc3796b004a200c79c9dd0f7bad0a5f4
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-03-16 06:30:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import './App.css';
+import Header from './Header';
+import SwipeButtons from './SwipeButtons';
+import TinderCards from './TinderCrads';
+
+function App() {
+  return (
+    <div className="app">
+
+      <Header />
+      <TinderCards />
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SwipeButtons.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./SwipeButtons.css"
+import ReplyIcon from '@mui/icons-material/Reply';
+import CloseIcon from '@mui/icons-material/Close';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import IconButton from '@mui/material/IconButton';
+
+const SwipeButtons = () => {
+    return (
+        <div className='swipeButtons'>
+            <IconButton className='swipeButtons__repeat'>
+                <ReplyIcon fontSize='large' />
+            </IconButton>
+            <IconButton className='swipeButtons__left'>
+                <CloseIcon fontSize='large' />
+            </IconButton>
+            <IconButton className='swipeButtons__star'>
+                <StarRateIcon fontSize='large' />
+            </IconButton>
+            <IconButton className='swipeButtons__right'>
+                <FavoriteIcon fontSize='large' />
+            </IconButton>
+            <IconButton className='swipeButtons__lightning'>
+                <FlashOnIcon fontSize='large' />
+            </IconButton>
+        </div>
+    )
+}
+
+export default SwipeButtons
